531ab945decb873198e75056f4d7ac9aad5ba9ca
zleun <jepark0702@gmail.com>
2024-07-22 00:06:14 +0900

    [Feat] #12 - mouseover 생성

--- Presentation/Profile/profile.js
+++ Presentation/Profile/profile.js
@@ -37,3 +37,22 @@ document.addEventListener('DOMContentLoaded', function() {
         </div>
     `).join('');
 });
+
+
+//mouseover
+        let gnb = document.querySelectorAll("#gnb > li")
+        let gnbElement = document.querySelector("#gnb")
+
+        for (let i = 0; i < gnb.length; i++) {
+            gnb[i].addEventListener("mouseover",() => {
+                gnbElement.classList.add("on")
+            })
+        }
+
+        let headerElement = document.querySelector("#header")
+
+        header.addEventListener("mouseout",(e) =>{
+            if (e.target.id == "gnb" ){
+                gnbElement.classList.remove("on")
+            }
+        })
