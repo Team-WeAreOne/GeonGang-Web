534c6ac2bb97588cf1524c260c1456f5960e76ed
realchaerin <cherries0210@naver.com>
2024-07-22 01:19:43 +0900

    [Feat] #22 - 하위메뉴바 수정

--- Presentation/Attendance/attendance.js
+++ Presentation/Attendance/attendance.js
@@ -20,3 +20,21 @@ function logout() {
 
   window.location.href = "../Main/main.html";
 }
+
+//mouseover
+let gnb = document.querySelectorAll("#gnb > li");
+let gnbElement = document.querySelector("#gnb");
+
+for (let i = 0; i < gnb.length; i++) {
+  gnb[i].addEventListener("mouseover", () => {
+    gnbElement.classList.add("on");
+  });
+}
+
+let headerElement = document.querySelector("#header");
+
+header.addEventListener("mouseout", (e) => {
+  if (e.target.id == "gnb") {
+    gnbElement.classList.remove("on");
+  }
+});
